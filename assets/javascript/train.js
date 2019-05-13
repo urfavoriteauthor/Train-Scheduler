f1adf295774d3be2d176b26e0dfbb36ebb27491f
URxFAVxAUTHOR <nygel.porterfield@gmail.com>
2019-05-12 23:57:03 -0400

    fina

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,85 +1,34 @@
 $(document).ready(function () {
     // var moment = require('moment');
 
-    var config = {
-        apiKey: "AIzaSyCs3K5zwuOuS0odq89IpPLC7HnXTOcDqgI",
-        authDomain: "recent-user-with-all-use-e8e76.firebaseapp.com",
-        databaseURL: "https://recent-user-with-all-use-e8e76.firebaseio.com",
-        projectId: "recent-user-with-all-use-e8e76",
-        storageBucket: ""
-      };
-  
-      firebase.initializeApp(config);
-  
-      var dataRef = firebase.database();
-    // moment().format();
+
+
+    
     var submitButton = $("#submitButton");
     var newTrain = $("#trainName");
     var trainTime = $("#trainFreq");
     var trainDestination = $("#trainDestination");
     var trainArrival = $("#trainArrival");
     var makeTrain = $("<tr></tr>");
-    var trainRow = $("tr");
+    var trainRow = $("<tr>");
 
     submitButton.on("click", function () {
-            var train = $(newTrain.val());
-            var time = $(trainTime.val());
-            var arrival = $("td") + $(trainArrival.val()) + $("td");
-            var destination = $(trainDestination.val());
-            console.log("New Train added")
-            console.log(train, destination, arrival, time)
-            $("tbody").append((
-                ("<td>" + train + "</td><td>" +
-                destination + "</td><td>" +
-                arrival + "</td><td>" +
-                time + "</td><td>"
+        event.preventDefault();
+        var train = $(newTrain.val());
+        var time = $(trainTime.val());
+        var arrival = $(trainArrival.val());
+        var destination = $(trainDestination.val());
+        console.log("New Train added")
+        console.log(train, destination, arrival, time)
+        trainRow();
+        $("tbody").append((
+                ("<td>" + train.text + "</td><td>" +
+                    destination.text + "</td><td>" +
+                    arrival.text + "</td><td>" +
+                    time.text + "</td><td>"
                 ))
-                dataRef.ref().push({
 
-                                train: train,
-                                time: time,
-                                arrival: arrival,
-                                destination: destination,
-                                dateAdded: firebase.database.ServerValue.TIMESTAMP
-                            });
-            );
-
-
-//         dataRef.ref().push({
-
-//             train: train,
-//             time: time,
-//             arrival: arrival,
-//             destination: destination,
-//             dateAdded: firebase.database.ServerValue.TIMESTAMP
-//         });
-//     });
-
-// // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-// dataRef.ref().on("child_added", function (childSnapshot) {
-
-//     // Log everything that's coming out of snapshot
-
-
-//     // full list of items to the well
-//     $("#full-member-list").append("<div class='well'><span class='member-name'> " +
-//         childSnapshot.val().name +
-//         " </span><span class='member-email'> " + childSnapshot.val().email +
-//         " </span><span class='member-age'> " + childSnapshot.val().age +
-//         " </span><span class='member-comment'> " + childSnapshot.val().comment +
-//         " </span></div>");
-
-//     // Handle the errors
-// }, function (errorObject) {
-//     console.log("Errors handled: " + errorObject.code);
-// });
-
-// dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
-//     // Change the HTML to reflect
-//     $("#name-display").text(snapshot.val().name);
-//     $("#email-display").text(snapshot.val().email);
-//     $("#age-display").text(snapshot.val().age);
-//     $("#comment-display").text(snapshot.val().comment);
-// });
-})
+        )
+        captureEvents();
+    })
 })
